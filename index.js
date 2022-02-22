686f140b7dfa6c3209f8f5594b3c8e988af39ce9
MarcusKruuleht <olen.marcus.lahe@gamil.com>
2022-02-22 15:00:55 +0200

    Created connection to database

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({extended: true}));
 
+// connect to database
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequalize')
+
+// testing connection
+sequalize
+	.authenticate()
+	.then(() => {
+		console.log('Connected to the database.');
+	}) ;
+	catch(err => {
+		console.error('Unable to connect to the database:', err);
+	});
+
 // simple route
 app.get("/", (req, res) => {
 	res.json({message: "Welcome to sequelize application." });
